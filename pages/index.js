335a1e3235675e7651b946181905a78529218df7
Vagner Batista <vagnereletronica@gmail.com>
2021-12-04 11:13:54 -0300

    Primeiro commit: Instalação do next, react, mui e emotion. Criação do .gitignore

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Index() {
+  return (
+    <div
+      style={{
+        backgroundColor: "yellow",
+        minHeight: "100px",
+        minWidth: "100px",
+      }}
+    >
+      Index em NextJs
+    </div>
+  );
+}
